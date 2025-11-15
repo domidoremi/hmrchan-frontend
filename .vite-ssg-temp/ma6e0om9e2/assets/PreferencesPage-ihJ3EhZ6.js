3c78cdcb0292c703631fe8c4b2e413d481c70b35
domidoremi <barnaice@gmail.com>
2025-11-16 06:16:25 +0900

    修正queue-dropdown / settings-dropdown 弹窗位置

--- .vite-ssg-temp/ma6e0om9e2/assets/PreferencesPage-ihJ3EhZ6.js
+++
@@ -1,6 +0,0 @@
-import "./api-services-BmQ9TwGt.js";
-import "./view-authorspage-B1NrczNS.js";
-import "./view-homepage-BmVNjp4X.js";
-import "./composables-CdbJX3Qi.js";
-import { t as PreferencesPage_default } from "./view-preferencespage-B4g_s_sj.js";
-export { PreferencesPage_default as default };
